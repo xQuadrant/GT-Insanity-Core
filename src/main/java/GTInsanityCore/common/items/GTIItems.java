3314c005787fdbed84de29adcd6769afbf11a677
xQuadrant <nexus6476@gmail.com>
2026-04-03 00:35:20 -0300

    Último commit da semana (Provavelmente)

--- src/main/java/GTInsanityCore/common/items/GTIItems.java
+++ src/main/java/GTInsanityCore/common/items/GTIItems.java
@@ -8,12 +8,30 @@ public class GTIItems {
 
     // Particle containment items
     public static ItemPlasmaContainer PLASMA_CONTAINER;
+    public static Item ELECTRIC_MOTOR_ULV;
+    public static Item ELECTRIC_PUMP_ULV;
+    public static Item CONVEYOR_MODULE_ULV;
+    public static Item ELECTRIC_PISTON_ULV;
+    public static Item ROBOT_ARM_ULV;
+    public static Item FIELD_GENERATOR_ULV;
+    public static Item EMITTER_ULV;
+    public static Item SENSOR_ULV;
+    public static Item FLUID_REGULATOR_ULV;
     //public static ItemStrata STRATA_ITEM;
     //public static ItemResearchToken RESEARCH_TOKEN;
 
     public static void init() {
         if (PLASMA_CONTAINER == null) {
             PLASMA_CONTAINER = createItem("plasma_container", new ItemPlasmaContainer());
+            ELECTRIC_MOTOR_ULV = createItem("electric_motor_ulv", new Item());
+            ELECTRIC_PUMP_ULV = createItem("electric_pump_ulv", new Item());
+            CONVEYOR_MODULE_ULV = createItem("conveyor_module_ulv", new Item());
+            ELECTRIC_PISTON_ULV = createItem("electric_piston_ulv", new Item());
+            ROBOT_ARM_ULV = createItem("robot_arm_ulv", new Item());
+            FIELD_GENERATOR_ULV = createItem("field_generator_ulv", new Item());
+            EMITTER_ULV = createItem("emitter_ulv", new Item());
+            SENSOR_ULV = createItem("sensor_ulv", new Item());
+            FLUID_REGULATOR_ULV = createItem("fluid_regulator_ulv", new Item());
         }
         //STRATA_ITEM = createItem("strata", new ItemStrata());
         //RESEARCH_TOKEN = createItem("research_token", new ItemResearchToken());
@@ -29,5 +47,14 @@ public class GTIItems {
     public static void registerItemBlocks(IForgeRegistry<Item> registry) {
         init();
         registry.register(PLASMA_CONTAINER);
+        registry.register(ELECTRIC_MOTOR_ULV);
+        registry.register(ELECTRIC_PUMP_ULV);
+        registry.register(CONVEYOR_MODULE_ULV);
+        registry.register(ELECTRIC_PISTON_ULV);
+        registry.register(ROBOT_ARM_ULV);
+        registry.register(FIELD_GENERATOR_ULV);
+        registry.register(EMITTER_ULV);
+        registry.register(SENSOR_ULV);
+        registry.register(FLUID_REGULATOR_ULV);
     }
 }
--- src/main/java/GTInsanityCore/common/metatileentities/GTIMetaTileEntities.java
+++ src/main/java/GTInsanityCore/common/metatileentities/GTIMetaTileEntities.java
@@ -2,8 +2,16 @@ package GTInsanityCore.common.metatileentities;
 
 import GTInsanityCore.GTInsanityCore;
 import gregtech.api.GTValues;
+import gregtech.api.metatileentity.SimpleGeneratorMetaTileEntity;
+import gregtech.api.metatileentity.SimpleMachineMetaTileEntity;
 import gregtech.api.metatileentity.multiblock.MultiblockAbility;
+import gregtech.api.recipes.RecipeMaps;
+import gregtech.api.util.GTUtility;
+import gregtech.client.renderer.texture.Textures;
 import gregtech.common.metatileentities.MetaTileEntities;
+import gregtech.common.metatileentities.electric.MetaTileEntityFisher;
+import gregtech.common.metatileentities.electric.MetaTileEntitySingleCombustion;
+import gregtech.common.metatileentities.electric.MetaTileEntitySingleTurbine;
 import net.minecraft.util.ResourceLocation;
 
 import java.util.Locale;
@@ -14,6 +22,7 @@ public final class GTIMetaTileEntities {
     private static final int LASER_OUTPUT_HATCH_BASE_ID = 32100;
     private static final int LASER_CONVERSION_ARRAY_ID = 32132;
     private static final int PRIMITIVE_BULK_SMELTER_ID = 32133;
+    private static final int ULV_MACHINE_BASE_ID = 32200;
 
     public static final MetaTileEntityCVD[] CVD_CHAMBERS = new MetaTileEntityCVD[GTValues.OpV + 1];
     public static final MetaTileEntityGTILaserOutputHatch[] LASER_OUTPUT_HATCHES =
@@ -61,5 +70,105 @@ public final class GTIMetaTileEntities {
                 PRIMITIVE_BULK_SMELTER_ID,
                 new MetaTileEntityPrimitiveBulkSmelterMultiblock(
                         new ResourceLocation(GTInsanityCore.MODID, "primitive_bulk_smelter")));
+
+        registerULVMachines();
+    }
+
+    private static void registerULVMachines() {
+        if (MetaTileEntities.ELECTRIC_FURNACE[GTValues.ULV] == null) {
+            MetaTileEntities.ELECTRIC_FURNACE[GTValues.ULV] = registerSimpleMachine(
+                    ULV_MACHINE_BASE_ID,
+                    "electric_furnace.ulv",
+                    RecipeMaps.FURNACE_RECIPES,
+                    Textures.ELECTRIC_FURNACE_OVERLAY,
+                    true);
+        }
+        if (MetaTileEntities.ASSEMBLER[GTValues.ULV] == null) {
+            MetaTileEntities.ASSEMBLER[GTValues.ULV] = registerSimpleMachine(
+                    ULV_MACHINE_BASE_ID + 1,
+                    "assembler.ulv",
+                    RecipeMaps.ASSEMBLER_RECIPES,
+                    Textures.ASSEMBLER_OVERLAY,
+                    true,
+                    GTUtility.hvCappedTankSizeFunction);
+        }
+        if (MetaTileEntities.WIREMILL[GTValues.ULV] == null) {
+            MetaTileEntities.WIREMILL[GTValues.ULV] = registerSimpleMachine(
+                    ULV_MACHINE_BASE_ID + 2,
+                    "wiremill.ulv",
+                    RecipeMaps.WIREMILL_RECIPES,
+                    Textures.WIREMILL_OVERLAY,
+                    true);
+        }
+        if (MetaTileEntities.ORE_WASHER[GTValues.ULV] == null) {
+            MetaTileEntities.ORE_WASHER[GTValues.ULV] = registerSimpleMachine(
+                    ULV_MACHINE_BASE_ID + 3,
+                    "ore_washer.ulv",
+                    RecipeMaps.ORE_WASHER_RECIPES,
+                    Textures.ORE_WASHER_OVERLAY,
+                    true);
+        }
+        if (MetaTileEntities.POLARIZER[GTValues.ULV] == null) {
+            MetaTileEntities.POLARIZER[GTValues.ULV] = registerSimpleMachine(
+                    ULV_MACHINE_BASE_ID + 4,
+                    "polarizer.ulv",
+                    RecipeMaps.POLARIZER_RECIPES,
+                    Textures.POLARIZER_OVERLAY,
+                    true);
+        }
+        if (MetaTileEntities.COMBUSTION_GENERATOR[GTValues.ULV] == null) {
+            MetaTileEntities.COMBUSTION_GENERATOR[GTValues.ULV] = registerGenerator(
+                    ULV_MACHINE_BASE_ID + 5,
+                    new MetaTileEntitySingleCombustion(
+                            new ResourceLocation(GTInsanityCore.MODID, "combustion_generator.ulv"),
+                            RecipeMaps.COMBUSTION_GENERATOR_FUELS,
+                            Textures.COMBUSTION_GENERATOR_OVERLAY,
+                            GTValues.ULV,
+                            GTUtility.genericGeneratorTankSizeFunction));
+        }
+        if (MetaTileEntities.STEAM_TURBINE[GTValues.ULV] == null) {
+            MetaTileEntities.STEAM_TURBINE[GTValues.ULV] = registerGenerator(
+                    ULV_MACHINE_BASE_ID + 6,
+                    new MetaTileEntitySingleTurbine(
+                            new ResourceLocation(GTInsanityCore.MODID, "steam_turbine.ulv"),
+                            RecipeMaps.STEAM_TURBINE_FUELS,
+                            Textures.STEAM_TURBINE_OVERLAY,
+                            GTValues.ULV,
+                            GTUtility.steamGeneratorTankSizeFunction));
+        }
+        if (MetaTileEntities.FISHER[GTValues.ULV] == null) {
+            MetaTileEntities.FISHER[GTValues.ULV] = MetaTileEntities.registerMetaTileEntity(
+                    ULV_MACHINE_BASE_ID + 7,
+                    new MetaTileEntityFisher(
+                            new ResourceLocation(GTInsanityCore.MODID, "fisher.ulv"),
+                            GTValues.ULV));
+        }
+    }
+
+    private static SimpleMachineMetaTileEntity registerSimpleMachine(int id, String name,
+                                                                     gregtech.api.recipes.RecipeMap<?> recipeMap,
+                                                                     gregtech.client.renderer.ICubeRenderer renderer,
+                                                                     boolean frontFacing) {
+        return registerSimpleMachine(id, name, recipeMap, renderer, frontFacing, GTUtility.defaultTankSizeFunction);
+    }
+
+    private static SimpleMachineMetaTileEntity registerSimpleMachine(int id, String name,
+                                                                     gregtech.api.recipes.RecipeMap<?> recipeMap,
+                                                                     gregtech.client.renderer.ICubeRenderer renderer,
+                                                                     boolean frontFacing,
+                                                                     java.util.function.Function<Integer, Integer> tankScaling) {
+        return MetaTileEntities.registerMetaTileEntity(
+                id,
+                new SimpleMachineMetaTileEntity(
+                        new ResourceLocation(GTInsanityCore.MODID, name),
+                        recipeMap,
+                        renderer,
+                        GTValues.ULV,
+                        frontFacing,
+                        tankScaling));
+    }
+
+    private static SimpleGeneratorMetaTileEntity registerGenerator(int id, SimpleGeneratorMetaTileEntity generator) {
+        return MetaTileEntities.registerMetaTileEntity(id, generator);
     }
 }
